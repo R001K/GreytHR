cbd6b5dadda0f07e1c9562490e7092222af02301
Rahul Khambra <112678535+R001K@users.noreply.github.com>
2024-10-06 00:01:44 +0530

    Update example.spec.js

--- tests/example.spec.js
+++ tests/example.spec.js
@@ -1,12 +1,12 @@
 const { test, expect } = require('@playwright/test');
 
 test('Greyt HR ', async ({ page }) => {
-  // await page.goto('https://panorama.greythr.com//');
-  // await page.locator('[name="username"]').fill('PSS094')
-  // await page.locator('[placeholder="Password"]').fill('pano90AA@@')
-  // await page.locator('[type="submit"]').click();
-  // await page.getByRole('button', { name: /Sign/ }).click();
-  // await page.locator('[class="toggle"]').first().click();
-  // await page.locator('[class="dropdown-item"]').nth(1).click();
- //await page.locator('gt-button').getByRole('button', { name: /Sign/ }).nth(1).click();      
+  await page.goto('https://panorama.greythr.com//');
+  await page.locator('[name="username"]').fill('PSS094')
+  await page.locator('[placeholder="Password"]').fill('pano90AA@@')
+  await page.locator('[type="submit"]').click();
+  await page.getByRole('button', { name: /Sign/ }).click();
+  await page.locator('[class="toggle"]').first().click();
+  await page.locator('[class="dropdown-item"]').nth(1).click();
+ await page.locator('gt-button').getByRole('button', { name: /Sign/ }).nth(1).click();      
 });
